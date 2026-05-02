086edd69d4f8cd795afaff90bf63d85c9316ca5d
Bektur13 <duyshembekov04@mail.ru>
2026-05-02 15:01:42 -0700

    Build UI

--- app/api/review/route.ts
+++ app/api/review/route.ts
@@ -8,16 +8,17 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
 
     const response = await genAi.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-3.1-pro",
       contents: `Review this ${body.language} code and return feedback as JSON with fields: bugs, improvements, style, summary. Code: ${body.code}`,
     });
     console.log(body);
 
-    const cleanResponse = await response.text?.replace(/```json|```/g, "").trim();
-    const parsed = JSON.parse(cleanResponse);
+    const cleanResponse = response.text?.replace(/```json|```/g, "").trim();
+    const parsed = JSON.parse(cleanResponse || "");
 
-    return NextResponse.json({ message: "it works" });
+    return NextResponse.json(parsed);
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error: "Somehing went wrong" }, { status: 500 });
   }
 }
